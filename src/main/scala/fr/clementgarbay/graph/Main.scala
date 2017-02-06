4aa63994ab8bb0855c4f34beccf88325be9267f3
Anael CHARDAN <anael.chardan@gmail.com>
2017-02-06 08:16:57 +0100

    Few lines of test

--- src/main/scala/fr/clementgarbay/graph/Main.scala
+++ src/main/scala/fr/clementgarbay/graph/Main.scala
@@ -1,17 +1,30 @@
 package fr.clementgarbay.graph
 
+import fr.clementgarbay.graph.Link._
 /**
   * @author Clément Garbay
   */
 object Main extends App {
 
   override def main(args: Array[String]): Unit = {
-//    val binaryHeap = BinaryHeap(List(4,10,6,12,14,9))
-//
-//    println(binaryHeap)
-//    println(binaryHeap.add(1))
-//    println(binaryHeap.removeRoot())
+    val link: Arc[Int] = Arc(1, 2, 4.0)
+    val link2: Arc[Int] = Arc(3, 4, 10.0)
+    val link3: Arc[Int] = Arc(3, 4, 6.0)
+    val link4: Arc[Int] = Arc(3, 4, 12.0)
+    val link5: Arc[Int] = Arc(3, 4, 14.0)
+    val link6: Arc[Int] = Arc(3, 4, 9.0)
 
+    println(link2 < link3)
+
+    val binaryHeap = BinaryHeap(List(4,10,6,12,14,9))
+
+    println(binaryHeap)
+    println(binaryHeap.add(1))
+    println(binaryHeap.removeRoot())
+
+    val binaryHeapArcs = BinaryHeap[Link[Int]](List(link, link2, link3, link4, link5, link6))
+    println(binaryHeapArcs)
+    println(binaryHeapArcs.add(Arc(5, 6, 1.0)))
 //    val graph1 = GraphTools.generateUndirectedGraph(10,10)
 
 
